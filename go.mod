0446ff00b88e3e8e4ed40433bae31ee564b93073
Sergei3232 <Sergei-bashmakov@yandex.ru>
2021-10-28 16:47:38 +0300

    init redis

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Sergei3232/Redis
+
+go 1.17
+
+require github.com/gomodule/redigo v1.8.5
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gomodule/redigo/redis"
+)
+
+func main() {
+	c, err := redis.Dial("tcp", "localhost:6379")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	psc := redis.PubSubConn{Conn: c}
+	psc.Subscribe("example")
+	for {
+		switch v := psc.Receive().(type) {
+		case redis.Message:
+			fmt.Printf("%s: message: %s\n", v.Channel, v.Data)
+		case redis.Subscription:
+			fmt.Printf("%s: %s %d\n", v.Channel, v.Kind, v.Count)
+		case error:
+			fmt.Println(v)
+		}
+
+	}
+}
